ed55dc3601464aab5d290be3a5a4c487e181cd04
yexiyue <yexiyue666@qq.com>
2022-04-12 07:51:32 -0400

    修复bug

--- src/neovim/neovim_install.sh
+++ src/neovim/neovim_install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 function neovimInstall(){
-echo -e "\034[1;5;34m==========开始安装neovim=========\033[0m"
-yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum install -y neovim python3-neovim
-echo -e "\034[1;5;32m=========安装完成=========\033[0m"
+echo -e "\033[1;5;34m==========开始安装neovim=========\033[0m"
+wget -O ~/neovim.tar.gz https://github.com/neovim/neovim/releases/download/v0.6.1/nvim-linux64.tar.gz
+mkdir -p /usr/local/neovim
+tar -xvf ~/neovim.tar.gz -C /usr/local/neovim --strip-components 1
+ln -s /usr/local/neovim/bin/nvim /bin/nvim
+echo -e "\033[1;5;32m=========安装完成=========\033[0m"
 pip install pynvim
-pip4 install pynvim
+pip3 install pynvim
 npm install -g neovim 
 python3 -m pip install neovim
 }
